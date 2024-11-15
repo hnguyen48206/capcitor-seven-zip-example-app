adc6800332e37912c72d528cfdc9b31ed5075229
hnguyen48206 <hoangbich1989@gmail.com>
2024-11-15 13:09:53 +0700

    abc

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,70 @@
 import { Component } from '@angular/core';
 import { BLEServ } from 'ble-srv';
+import { Preferences } from '@capacitor/preferences';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage  {
-  
+export class HomePage {
+  addDeviceID = '';
+  rmDeviceID = '';
+  currentList: string | null = '';
+  arr: any[] = [];
+  updateInterval: any;
+  ionViewWillEnter() {
+    this.getCurrentList();
+  }
+
   async startBLESER() {
     await BLEServ.startService();
+    this.updateInterval = setInterval(() => {
+      this.getCurrentList();
+    }, 2000);
   }
   async stopBLESER() {
     await BLEServ.stopService();
+    if (this.updateInterval != null) clearInterval(this.updateInterval);
+  }
+  async getCurrentList() {
+    let res = await Preferences.get({ key: 'MacBluetoothsConnected' });
+    this.currentList = res.value;
+    this.arr = JSON.parse(this.currentList ? this.currentList : '[]');
+    console.log(this.currentList);
+  }
+  async setNewDevice() {
+    if (this.isValidMACAddress(this.addDeviceID)) {
+      //add
+      this.arr.push({
+        mac: this.addDeviceID,
+        vehicleID: 'ABC',
+        status: 'on',
+      });
+      await Preferences.set({
+        key: 'MacBluetoothsConnected',
+        value: JSON.stringify(this.arr),
+      });
+      alert('Add OK');
+      this.addDeviceID = '';
+      this.getCurrentList();
+    }
+  }
+  async rmDevice() {
+    if (this.isValidMACAddress(this.rmDeviceID)) {
+      //rm
+      this.arr = this.arr.filter((device) => device.mac != this.rmDeviceID);
+      await Preferences.set({
+        key: 'MacBluetoothsConnected',
+        value: JSON.stringify(this.arr),
+      });
+      alert('Rm OK');
+      this.rmDeviceID = '';
+      this.getCurrentList();
+    }
+  }
+  isValidMACAddress(mac: string) {
+    const macRegex = /^([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}$/;
+    return macRegex.test(mac);
   }
 }
