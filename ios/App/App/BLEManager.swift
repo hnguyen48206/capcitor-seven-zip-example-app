7f6d6be893999b034052650732f94ca7863ba3df
hnguyen48206 <hoangbich1989@gmail.com>
2024-12-11 10:24:04 +0700

    abc

--- ios/App/App/BLEManager.swift
+++ ios/App/App/BLEManager.swift
@@ -28,8 +28,6 @@ enum BluetoothCommand: String, CaseIterable {
   case readAll = "READ_ALL\r"
 }
 
-
-
 @available(iOS 14.0, *)
 class BLEManager: NSObject, CBCentralManagerDelegate, CLLocationManagerDelegate{
   private let locationManager = CLLocationManager()
@@ -76,16 +74,17 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CLLocationManagerDelegate{
     locationManager.requestAlwaysAuthorization()
     locationManager.allowsBackgroundLocationUpdates = true
     locationManager.pausesLocationUpdatesAutomatically = false
-    locationManager.showsBackgroundLocationIndicator = true
+    locationManager.showsBackgroundLocationIndicator = false
     locationManager.startUpdatingLocation()
     locationManager.startMonitoringSignificantLocationChanges()
   }
   func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
   { guard let location = locations.last
     else { return }
-    print("Updated Location: \(location)")
+    printLog(msg: "Updated Location: \(location)")
   }
-  func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) { print("Failed to get location: \(error)")
+  func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+    printLog(msg: "Failed to get location: \(error)")
   }
   
   func scanHistoryLog(isGet:Bool)
@@ -117,23 +116,23 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CLLocationManagerDelegate{
     MacBluetoothsConnectedStr = UserDefaults.standard.string(forKey: "CapacitorStorage.MacBluetoothsConnected") ?? ""
     BLEConfigsStr = UserDefaults.standard.string(forKey: "CapacitorStorage.BLEConfigs") ?? ""
     Vehicle_IsMovingStr = UserDefaults.standard.string(forKey: "CapacitorStorage.Vehicle_IsMoving") ?? ""
-    print("MacBluetoothsConnectedStr \(String(describing: MacBluetoothsConnectedStr))")
+    printLog(msg: "MacBluetoothsConnectedStr \(String(describing: MacBluetoothsConnectedStr))")
     do {
       if(MacBluetoothsConnectedStr != "")
       {
         guard let MacBluetoothsConnectedData = MacBluetoothsConnectedStr?.data(using: .utf8) else {
-          print("Unable to convert MacBluetoothsConnectedStr to data")
+          printLog(msg: "Unable to convert MacBluetoothsConnectedStr to data")
           return
         }
         //print("MacBluetoothsConnectedData \(MacBluetoothsConnectedData)")
         listOfSavedDevice = try JSONDecoder().decode([BLEDevice].self, from: MacBluetoothsConnectedData)
-        print("listOfSavedDevice \(listOfSavedDevice.description)")
+        printLog(msg: "listOfSavedDevice \(listOfSavedDevice.description)")
       }
       
       if(BLEConfigsStr != "")
       {
         guard let BLEConfigsData = BLEConfigsStr?.data(using: .utf8) else {
-          print("[DEBUG] - Unable to convert BLEConfigsStr to data")
+          printLog(msg: "[DEBUG] - Unable to convert BLEConfigsStr to data")
           return
         }
         BLEConfigs = try JSONDecoder().decode(BLEConfig.self, from: BLEConfigsData)
@@ -142,14 +141,14 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CLLocationManagerDelegate{
       if(Vehicle_IsMovingStr != "")
       {
         guard let Vehicle_IsMovingData = Vehicle_IsMovingStr?.data(using: .utf8) else {
-          print("[DEBUG] - Unable to convert Vehicle_IsMovingStr to data")
+          printLog(msg: "[DEBUG] - Unable to convert Vehicle_IsMovingStr to data")
           return
         }
         Vehicle_IsMoving = try JSONDecoder().decode(VehicleIsMoving.self, from: Vehicle_IsMovingData)
       }
       
     } catch {
-      print("[DEBUG] - Failed to decode JSON: \(error.localizedDescription)")
+      printLog(msg: "[DEBUG] - Failed to decode JSON: \(error.localizedDescription)")
     }
     
     SCAN_PERIOD = TimeInterval(round(Double(BLEConfigs.scan_period)/1000))
@@ -160,15 +159,15 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CLLocationManagerDelegate{
     switch central.state {
     case .poweredOn:
       startScanningInForeground()
-      print("[DEBUG] - Start Scanning From Load")
-      os_log("[DEBUG] - Start Scanning From Load", log: OSLog.default, type: .debug)
+      printLog(msg: "[DEBUG] - Start Scanning From Load")
+//      os_log("[DEBUG] - Start Scanning From Load", log: OSLog.default, type: .debug)
       blSettingStatus = true
     case .poweredOff, .unauthorized, .unsupported, .unknown, .resetting:
-      print("[DEBUG] - Bluetooth is not available.")
-      os_log("[DEBUG] - Bluetooth is not available.", log: OSLog.default, type: .debug)
+      printLog(msg: "[DEBUG] - Bluetooth is not available.")
+//      os_log("[DEBUG] - Bluetooth is not available.", log: OSLog.default, type: .debug)
       blSettingStatus = false
     @unknown default:
-      print("[DEBUG] - A new state is available that is not handled.")
+      printLog(msg: "[DEBUG] - A new state is available that is not handled.")
     }
   }
   
@@ -179,7 +178,7 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CLLocationManagerDelegate{
       listOfLatestSans.removeFirst()
     }
     let newItem = df.string(from: Date()) + "_" + deviceList
-    print(newItem)
+//    printLog(msg: newItem)
     listOfLatestSans.append(newItem)
     scanHistoryLog(isGet: false)
   }
@@ -197,13 +196,14 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CLLocationManagerDelegate{
       reloadLocalStorage()
       if(Vehicle_IsMoving.Vehicle_IsMoving && blSettingStatus && !isTargetDeviceConnected())
       {
+        getCurrentConnectedList()
         centralManager.scanForPeripherals(withServices: listOfBLEServ, options: options)
-        os_log("[DEBUG] - Start Scanning in BG", log: OSLog.default, type: .debug)
       }
     }
     else
     {
-      os_log("[DEBUG] - No Scanning in BG cause the FG scan is happening", log: OSLog.default, type: .debug)
+      printLog(msg: "[DEBUG] - No Scanning in BG cause the FG scan is happening")
+//      os_log("[DEBUG] - No Scanning in BG cause the FG scan is happening", log: OSLog.default, type: .debug)
     }
   }
   
@@ -216,14 +216,15 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CLLocationManagerDelegate{
           CBCentralManagerScanOptionAllowDuplicatesKey: true,
           CBConnectPeripheralOptionNotifyOnConnectionKey: true
         ]
-        print("[DEBUG] - Start Scanning in FG")
+        self.printLog(msg: "[DEBUG] - Start Scanning in FG")
         self.reloadLocalStorage()
         self.isScanning = true;
+        self.getCurrentConnectedList()
         self.centralManager.scanForPeripherals(withServices: self.listOfBLEServ, options: options)
       }
       else if(self.Vehicle_IsMoving.Vehicle_IsMoving && !self.isFG && self.blSettingStatus && !self.isTargetDeviceConnected())
       {
-        print("[DEBUG] - Start Scanning in BG plus")
+        self.printLog(msg: "[DEBUG] - Start Scanning in BG plus")
         let options: [String: Any] = [
           CBCentralManagerScanOptionAllowDuplicatesKey: true,
           CBConnectPeripheralOptionNotifyOnConnectionKey: true,
@@ -231,11 +232,12 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CLLocationManagerDelegate{
         ]
         self.reloadLocalStorage()
         self.isScanning = true;
+        self.getCurrentConnectedList()
         self.centralManager.scanForPeripherals(withServices: self.listOfBLEServ, options: options)
       }
       else
       {
-        print("[DEBUG] - Not moving \(self.Vehicle_IsMoving.Vehicle_IsMoving)) - No BL \(self.blSettingStatus)")
+        self.printLog(msg: "[DEBUG] - Not moving \(self.Vehicle_IsMoving.Vehicle_IsMoving)) - No BL \(self.blSettingStatus)")
       }
       self.timer.executeAfterDelay(delay: self.SCAN_PERIOD) {
         self.stopScanningInForeground(autorestart: true)
@@ -247,8 +249,8 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CLLocationManagerDelegate{
     isScanning = false
     centralManager.stopScan()
     updateDeviceStatus()
-    //    print("Stop Scanning")
-    os_log("[DEBUG] - Stop Scanning in BG", log: OSLog.default, type: .debug)
+    printLog(msg: "[DEBUG] - Stop Scanning in BG")
+//    os_log("[DEBUG] - Stop Scanning in BG", log: OSLog.default, type: .debug)
   }
   
   public func stopScanningInForeground(autorestart:Bool) {
@@ -258,7 +260,7 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CLLocationManagerDelegate{
       centralManager.stopScan()
       updateDeviceStatus()
     }
-    print("[DEBUG] - Stop Scanning in FG or BG plus \(autorestart)")
+    printLog(msg: "[DEBUG] - Stop Scanning in FG or BG plus \(autorestart)")
     //    os_log("[DEBUG] - Stop Scanning in Foreground", log: OSLog.default, type: .debug)
     if(autorestart)
     {
@@ -272,7 +274,8 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CLLocationManagerDelegate{
   {
     if(targetDevice != nil && targetDevice?.state.rawValue == 2)
     {
-      os_log("[DEBUG] - Target Device is already in connection - No scan needed", log: OSLog.default, type: .debug)
+//      os_log("[DEBUG] - Target Device is already in connection - No scan needed", log: OSLog.default, type: .debug)
+      printLog(msg: "[DEBUG] - Target Device is already in connection - No scan needed")
       return true
     }
     else
@@ -313,12 +316,12 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CLLocationManagerDelegate{
         UserDefaults.standard.set(jsonString, forKey: "CapacitorStorage.MacBluetoothsConnected")
         pushLocalNoti(msg: jsonString!)
         addScanLogHistory(deviceList: jsonString!)
-        timer.executeAfterDelay(delay: self.CONNECT_DELAY) {
-          print("[DEBUG] - DELAY BEFORE CONNECTION \(self.CONNECT_DELAY)")
-          self.connectDevice()
-        }
+//        timer.executeAfterDelay(delay: self.CONNECT_DELAY) {
+//          self.printLog(msg: "[DEBUG] - DELAY BEFORE CONNECTION \(self.CONNECT_DELAY)")
+//          self.connectDevice()
+//        }
       } catch {
-        print("[DEBUG] - Failed to encode devices: \(error.localizedDescription)")
+        printLog(msg: "[DEBUG] - Failed to encode devices: \(error.localizedDescription)")
       }
     }
     else
@@ -331,9 +334,7 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CLLocationManagerDelegate{
     // Handle discovered peripheral
     //    self.count+=1
     let msg = "[DEBUG] - \(self.count) - Discovered \(peripheral.name ?? "unknown device") \(peripheral.identifier.uuidString) \(peripheral.state)"
-    print(msg)
-    os_log("[DEBUG] DEVICE FOUND", log: OSLog.default, type: .debug)
-    
+    printLog(msg: msg)
     detectedDevices.insert(peripheral.identifier.uuidString)
     checkIfTargetDeviceToConnect(peripheral: peripheral)
   }
@@ -358,17 +359,18 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CLLocationManagerDelegate{
     }
     else
     {
-      os_log("[DEBUG] TARGET DEVICE is alreay in connection", log: OSLog.default, type: .debug)
+      printLog(msg: "[DEBUG] TARGET DEVICE is alreay in connection")
+//      os_log("[DEBUG] TARGET DEVICE is alreay in connection", log: OSLog.default, type: .debug)
     }
   }
   
   func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-    print("[DEBUG] - Connected to \(peripheral.name ?? "Unknown")")
+    printLog(msg: "[DEBUG] - Connected to \(peripheral.name ?? "Unknown")")
     peripheral.discoverServices(nil)
   }
   
   func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
-    print("[DEBUG] - Failed to connect to \(peripheral.name ?? "Unknown"): \(error?.localizedDescription ?? "No error information")")
+    printLog(msg: "[DEBUG] - Failed to connect to \(peripheral.name ?? "Unknown"): \(error?.localizedDescription ?? "No error information")")
   }
   
   func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
@@ -390,84 +392,102 @@ class BLEManager: NSObject, CBCentralManagerDelegate, CLLocationManagerDelegate{
       }
     }
   }
-    func scheduleBLEScan() {
-      //    let request = BGAppRefreshTaskRequest(identifier: "com.hnguyen48206.blesrv.ios")
-      countPendingTask()
-      let request = BGProcessingTaskRequest(identifier: "com.hnguyen48206.blesrv.ios")
-      request.requiresNetworkConnectivity = false
-      request.requiresExternalPower = false
-      request.earliestBeginDate = Date(timeIntervalSinceNow: 60.0)
-      do {
-        try BGTaskScheduler.shared.submit(request)
-        logger.log("[DEBUG] - Registered next schedule.")
-      } catch {
-        print("[DEBUG] - Could not schedule BLE scan: \(error)")
-        logger.log("[DEBUG] - Could not schedule BLE scan: \(error)")
-      }
+  func scheduleBLEScan() {
+    //    let request = BGAppRefreshTaskRequest(identifier: "com.hnguyen48206.blesrv.ios")
+    countPendingTask()
+    let request = BGProcessingTaskRequest(identifier: "com.hnguyen48206.blesrv.ios")
+    request.requiresNetworkConnectivity = false
+    request.requiresExternalPower = false
+    request.earliestBeginDate = Date(timeIntervalSinceNow: 60.0)
+    do {
+      try BGTaskScheduler.shared.submit(request)
+      logger.log("[DEBUG] - Registered next schedule.")
+    } catch {
+      printLog(msg: "[DEBUG] - Could not schedule BLE scan: \(error)")
+      logger.log("[DEBUG] - Could not schedule BLE scan: \(error)")
     }
-    
-    func requestLocalNotification() {
-      UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
-        if granted {
-          print("[DEBUG] - Permission granted")
-        } else if let error = error {
-          print("[DEBUG] - Permission denied: \(error.localizedDescription)")
-        }
-      }
-    }
-    
-    func pushLocalNoti(msg: String)
-    {
-      if(BLEConfigs.isTesting)
-      {
-        let content = UNMutableNotificationContent()
-        content.title = "BLE Scanning"
-        content.body = msg
-        content.sound = nil
-        content.categoryIdentifier = "silentCategory"
-        
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
-        let id = "hnguyen48206"
-        let request = UNNotificationRequest(identifier: id, content: content, trigger: trigger)
-        let center = UNUserNotificationCenter.current()
-        center.removeDeliveredNotifications(withIdentifiers: [id])
-        center.removePendingNotificationRequests(withIdentifiers: [id])
-        
-        center.add(request) { error in
-          if let error = error {
-            print("[DEBUG] - Error adding notification: \(error.localizedDescription)")
-          }
-        }
-      }
-      else
-      {
-        print("[DEBUG] - No testing mode")
-      }
-    }
-    
-    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
-      if let error = error { print("Disconnected from peripheral \(peripheral.name ?? "Unknown") with error: \(error.localizedDescription)") }
-      else { print("Disconnected from peripheral \(peripheral.name ?? "Unknown") successfully") }
-    }
-    
-    func countPendingTask()
-    {
-      BGTaskScheduler.shared.getPendingTaskRequests { (taskRequests) in
-        let pendingTaskCount = taskRequests.count
-        print("Number of pending task requests: \(pendingTaskCount)") }
-    }
-    
-    //  func writeValue() {
-    //
-    //    if (isTargetDeviceConnected()) { //check if myPeripheral is connected to send data
-    //          BluetoothCommand.allCases.forEach {
-    //              let dataToSend: Data = $0.rawValue.data(using: .utf8)!
-    //            targetDevice?.writeValue(dataToSend, for: myCharacteristic, type: .withResponse)
-    //          }
-    //
-    //      } else {
-    //          print("Not connected")
-    //      }
-    //
-    //  }
   }
+  
+  func requestLocalNotification() {
+    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+      if granted {
+        self.printLog(msg: "[DEBUG] - Permission granted")
+      } else if let error = error {
+        self.printLog(msg: "[DEBUG] - Permission denied: \(error.localizedDescription)")
+      }
+    }
+  }
+  
+  func pushLocalNoti(msg: String)
+  {
+    if(BLEConfigs.isTesting)
+    {
+      let content = UNMutableNotificationContent()
+      content.title = "BLE Scanning"
+      content.body = msg
+      content.sound = nil
+      content.categoryIdentifier = "silentCategory"
+      
+      let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+      let id = "hnguyen48206"
+      let request = UNNotificationRequest(identifier: id, content: content, trigger: trigger)
+      let center = UNUserNotificationCenter.current()
+      center.removeDeliveredNotifications(withIdentifiers: [id])
+      center.removePendingNotificationRequests(withIdentifiers: [id])
+      
+      center.add(request) { error in
+        if let error = error {
+          self.printLog(msg: "[DEBUG] - Error adding notification: \(error.localizedDescription)")
+        }
+      }
+    }
+    else
+    {
+      printLog(msg: "[DEBUG] - No testing mode")
+    }
+  }
+  
+  func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+    if let error = error { printLog(msg: "Disconnected from peripheral \(peripheral.name ?? "Unknown") with error: \(error.localizedDescription)") }
+    else { printLog(msg: "Disconnected from peripheral \(peripheral.name ?? "Unknown") successfully") }
+  }
+  
+  func countPendingTask()
+  {
+    BGTaskScheduler.shared.getPendingTaskRequests { (taskRequests) in
+      let pendingTaskCount = taskRequests.count
+      self.printLog(msg: "Number of pending task requests: \(pendingTaskCount)") }
+  }
+  
+  //  func writeValue() {
+  //
+  //    if (isTargetDeviceConnected()) { //check if myPeripheral is connected to send data
+  //          BluetoothCommand.allCases.forEach {
+  //              let dataToSend: Data = $0.rawValue.data(using: .utf8)!
+  //            targetDevice?.writeValue(dataToSend, for: myCharacteristic, type: .withResponse)
+  //          }
+  //
+  //      } else {
+  //          print("Not connected")
+  //      }
+  //
+  //  }
+  
+  public func printLog(msg:String)
+  {
+    if(BLEConfigs.isTesting)
+    {
+      print(msg)
+    }
+  }
+  func getCurrentConnectedList()
+  {
+    let peripherals = centralManager.retrieveConnectedPeripherals(withServices: listOfBLEServ)
+    for peripheral in peripherals {
+      let msg = "[DEBUG] - \(self.count) - Discovered CONNECTED \(peripheral.name ?? "unknown device") \(peripheral.identifier.uuidString) \(peripheral.state)"
+      printLog(msg: msg)
+      detectedDevices.insert(peripheral.identifier.uuidString)
+      checkIfTargetDeviceToConnect(peripheral: peripheral)
+    }
+  }
+}
